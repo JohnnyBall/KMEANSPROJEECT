930fca414a279c7adab9aa732acbfc13beded173
John Ball <johneball93@gmail.com>
2016-05-09 10:39:12 -0400

    FrameforKmeans graph

---
+++ KMeanFrame.java
@@ -0,0 +1,232 @@
+/* Name:   John Ball
+ * Class:  Data Mining
+ * Project 2
+ */
+import java.io.*;
+import java.awt.*;
+import java.net.*;
+import java.lang.*;
+import java.util.*;
+import javax.swing.*;
+import java.applet.*;
+import javafx.scene.*;
+import java.awt.event.*;
+import java.util.Vector;
+import java.util.Random;
+import javax.swing.text.*;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javax.swing.event.*;
+import javax.swing.JComponent;
+import javafx.scene.chart.XYChart;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.ScatterChart;
+
+public class KMeanFrame extends JFrame implements ActionListener
+{
+    JButton   initializeButton;
+    JButton   nextButton;
+    JSpinner  klusterSpinner;
+    JLabel    klusterLabel;
+    JPanel    bottomPanel;
+    JFXPanel  graphPanel;
+    Container cp;
+    int       numOfMeans;
+
+    Vector<Vector<Point>> clusterVector;
+    Vector<Point> meanVector;
+
+    public KMeanFrame()
+    {
+        System.out.println("initializing kmeanframe");
+        klusterSpinner   = new JSpinner(new SpinnerNumberModel(2,2,5,1));
+        klusterLabel     = new JLabel(" K=");
+        nextButton       = new JButton("Next");
+        initializeButton = new JButton("Initialize");
+        bottomPanel      = new JPanel(new FlowLayout());
+        graphPanel       = new JFXPanel();
+
+        nextButton.setActionCommand ("NEXT");
+        nextButton.addActionListener(this);
+
+        initializeButton.setActionCommand ("INITIALIZE");
+        initializeButton.addActionListener(this);
+
+        bottomPanel.add(initializeButton);
+        bottomPanel.add(nextButton);
+        bottomPanel.add(klusterLabel);
+        bottomPanel.add(klusterSpinner);
+
+        cp = getContentPane();
+        cp.add(graphPanel, BorderLayout.CENTER);
+        cp.add(bottomPanel, BorderLayout.SOUTH);
+        setupMainFrame();
+    }//end of start
+
+//=================================================================================================================
+  public void setupMainFrame()
+  {
+    Toolkit   tk = Toolkit.getDefaultToolkit();
+    Dimension d  = tk.getScreenSize();
+    this.setSize(400, 500);
+    this.setMinimumSize(new Dimension(310, 300));
+    this.setLocation(d.width/4, d.height/4);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setTitle("K-Means Frame");
+    this.pack();
+    this.setVisible(true);
+  }// end of setupMainFrame
+//=================================================================================================================
+  public void actionPerformed(ActionEvent e)
+  {
+     if(e.getActionCommand().equals("INITIALIZE"))
+     {
+        initializePoints();
+        chartUpdate();
+     }
+     else if(e.getActionCommand().equals("NEXT"))
+     {
+        kmeansAlg();
+        chartUpdate();
+     }
+  }//end of Action performed
+//=================================================================================================================
+  /*This function will setup the random points whenever initialize is pressed
+   *
+   */
+  public void initializePoints()
+  {
+    Vector<Point> clusterValues = new Vector<Point>();
+    Point  tmpPoint;
+    Random rand = new Random();
+
+    while((clusterValues.size() < 20))
+    {
+        //System.out.println("clusterValues: "+ clusterValues.size());
+        tmpPoint = new Point(rand.nextInt(20),rand.nextInt(20));
+        if(!clusterValues.contains(tmpPoint))
+        {
+            clusterValues.add(tmpPoint);
+            //System.out.println("Added "+tmpPoint +" to clusterYValues.");
+        }
+    }// end of while
+
+    meanVector = new Vector<Point>();
+    numOfMeans = (int)klusterSpinner.getValue();
+    while(meanVector.size() < numOfMeans)
+    {
+        //System.out.println("meanVector: "+ meanVector.size());
+        tmpPoint = clusterValues.get(rand.nextInt(20));
+        if(!meanVector.contains(tmpPoint))
+        {
+          meanVector.add(tmpPoint);
+          //System.out.println("Added "+tmpPoint +" to meanVector.");
+        }
+    }
+
+    clusterVector = new Vector<Vector<Point>>();
+    clusterVector.add(clusterValues);
+
+    System.out.println("clusterValues: "+ clusterValues.toString());
+  }// ennd of initialize points
+//=================================================================================================================
+  public void kmeansAlg()
+  {
+    int minIndex;
+    double minValue;
+    double kmeansCalc;
+    Point  tmpPoint;
+    Point  tmpMeanPoint;
+    for(int i = 0; i < clusterVector.size(); i++)// iterates through the vectors of clusters
+        {
+            System.out.println("clusterVector.get(i).size(): "+clusterVector.get(i).size());
+            for(int j = 0; j < clusterVector.get(i).size();j++)// iterates through the vector of vector points
+            {
+               tmpPoint = clusterVector.get(i).get(j);
+               minValue = 100.0;
+               minIndex = 0;
+               for(int k = 0; k < meanVector.size();k++)
+               {
+                  tmpMeanPoint = meanVector.get(k);
+                  System.out.println("tmpPoint: "+tmpPoint.toString());
+                  System.out.println("tmpMeanPoint: "+tmpMeanPoint.toString());
+                  kmeansCalc = Math.sqrt(((tmpPoint.getX()-tmpMeanPoint.getX())*(tmpPoint.getX()-tmpMeanPoint.getX()))+
+                                            ((tmpPoint.getY()-tmpMeanPoint.getY())*(tmpPoint.getY()-tmpMeanPoint.getY())));
+                  System.out.println("kmeansCalc: "+kmeansCalc);
+                  if(minValue > kmeansCalc)
+                  {
+                    System.out.println("NEW K MEANS FOR CLUSTER "+ k+"!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+                    System.out.println("replacing minIndex:"+minIndex+ " with: "+k);
+                    minIndex = k;
+                    System.out.println("replacing minValue:"+minValue+ " with: "+kmeansCalc);
+                    minValue = kmeansCalc;
+                  }
+                  //
+               }// end of meanVector for loop
+
+            }// end of for(int j
+        }//end of for(int i
+
+  }//end of kmeansAlg()
+//=================================================================================================================
+  public void chartUpdate()
+  {
+        XYChart.Series tmpSeries;
+        XYChart.Series meanSeries;
+
+        System.out.println("Initialize axis");
+        final NumberAxis xAxis = new NumberAxis(0, 20, 1);
+        final NumberAxis yAxis = new NumberAxis(0, 20, 1);
+        Point tmpPoint;
+        Vector <XYChart.Series> seriesVector;
+        System.out.println("Setting up scatter chart");
+        ScatterChart<Number,Number> scatterChart = new ScatterChart<Number,Number>(xAxis,yAxis);
+
+        xAxis.setLabel("X-Values");
+        yAxis.setLabel("Y-Values");
+        scatterChart.setTitle("K-Means Chart");
+
+		seriesVector = new Vector<XYChart.Series>();
+
+		for(int i = 0; i < clusterVector.size(); i++)// iterates through the list of series points and vectors
+        {
+            tmpSeries = new XYChart.Series();
+            tmpSeries.setName("cluster " + i);
+            System.out.println("clusterVector.get(i).size(): "+clusterVector.get(i).size());
+            for(int j = 0; j < clusterVector.get(i).size();j++)// iterates through the vector of vector points
+            {
+             tmpPoint = clusterVector.get(i).get(j);
+             System.out.println("tmpPoint: "+tmpPoint.toString());
+             tmpSeries.getData().add(new XYChart.Data(tmpPoint.getX(), tmpPoint.getY()));//series1.getData().add(new XYChart.Data(4.2, 19.2));
+            }
+            seriesVector.add(i,tmpSeries);
+        }
+
+        meanSeries = new XYChart.Series();
+        meanSeries.setName("Mean cluster");
+        System.out.println("seriesVector.size(): "+seriesVector.size());
+        for(int i =0; i < seriesVector.size(); i++)// adds the points to the graph
+        {
+            scatterChart.getData().add(seriesVector.get(i));
+        }
+        scatterChart.getData().add(meanSeries);
+        Scene scene  = new Scene(scatterChart, 500, 400);
+        graphPanel.setScene(scene);
+  }// end of chartUpdate
+//=================================================================================================================
+
+  public static void main (String args[])
+  {
+    System.out.println("Starting Application...");
+    SwingUtilities.invokeLater(new Runnable()
+    {
+        @Override public void run()
+       {
+            new KMeanFrame();
+        }
+    });
+    System.out.println("Started");
+  }
+//=================================================================================================================
+}// class end
